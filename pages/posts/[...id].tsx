712cf3719622438bf889f605fa205b6b7c94d13b
Alessandro Ceruti <alessando@fido.id>
2022-11-23 11:38:07 +0100

    next end part 5

--- pages/posts/[...id].tsx
+++ pages/posts/[...id].tsx
@@ -1,17 +1,36 @@
-import { useRouter } from "next/router";
 import { getData } from "../../utils/functions";
 
 const SingoloArticolo = (props: any) => {
   return (
     <div>
-      <h2></h2>
-      <p></p>
+      <h2>{props.title}</h2>
+      <p>{props.content}</p>
     </div>
   );
 };
 
 export async function getStaticProps(context: any) {
-  console.log(context);
+  const useParams = context.params.id;
+  const data = await getData();
+  const post = data.posts.find((post: any) => post.id === useParams[0]);
+  if (!post) {
+    return {
+      notFound: true,
+    };
+  }
+  return {
+    props: post,
+  };
+}
+
+export async function getStaticPaths() {
+  const data = await getData();
+  const ids = data.posts.map((post: any) => post.id);
+  const paths = ids.map((id: any) => ({ params: { id: [id] } }));
+  return {
+    paths: paths,
+    fallback: true,
+  };
 }
 
 export default SingoloArticolo;
