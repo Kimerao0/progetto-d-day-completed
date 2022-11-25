dbd499271e1624f4e809a3b439fd072f9de03378
Alessandro Ceruti <alessando@fido.id>
2022-11-25 09:04:29 +0100

    Next last update

--- pages/api/create-post.ts
+++ pages/api/create-post.ts
@@ -3,21 +3,6 @@ import { dbConnection } from "../../utils/util";
 import { NextApiRequest, NextApiResponse } from "next";
 import { Post } from "../../dto/post";
 
-async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === "POST") {
-    const body = JSON.parse(req.body);
-    const newPost: Post = {
-      title: body.title,
-      content: body.content,
-      image: body.image,
-      author: body.author,
-    };
-    const client = await MongoClient.connect(dbConnection);
-    const db = client.db();
-    await db.collection("posts").insertOne({ ...newPost });
-    client.close();
-    res.status(201).json({ message: "Post created!" });
-  }
-}
+async function handler(req: NextApiRequest, res: NextApiResponse) {}
 
 export default handler;
